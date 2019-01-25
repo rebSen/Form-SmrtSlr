079f93ea236a400421d5e2ad2dd46b4921f9719a
Reb Sen <archivesreb@gmail.com>
2019-01-25 16:12:14 +0100

    cleaning imports in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-//import { Input, Row, Col, Select, Button, List  } from 'antd';
-//import 'antd/lib/date-picker/style/css';
-//import './App.css';
 import 'antd/dist/antd.css'; 
-import Form from'./components/Form'
-//import FormLess from './components/FormLess';
-
-
+import Form from'./components/Form';
 
 
 export default class App extends Component {
